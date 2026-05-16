7923928afc44aab85f94fea79fd5131e17b542a7
ashokishere <46769524+ashokishere@users.noreply.github.com>
2026-05-16 13:38:16 -0700

    initial Commit

---
+++ videos.ts
@@ -0,0 +1,45 @@
+export interface Video {
+  id: string;
+  title: string;
+  url: string;
+  tags: string[];
+}
+
+export const initialVideos: Video[] = [
+  {
+    id: "1PrQ25KkCdU",
+    title: "“A Satguru’s Promise: ‘To Those who Think Me Near, I am Near’” (in Hindi) | By Swami Chaitanyananda",
+    url: "https://www.youtube.com/watch?v=1PrQ25KkCdU",
+    tags: ["Swami Chaitanyananda", "Hindi", "2025", "Guru", "YSS", "1:01:54"]
+  },
+  {
+    id: "zox8ZTFb6VI",
+    title: "“At the Feet of the Guru”— Studying the New SRF Lessons | 2020 SRF Online World Convocation",
+    url: "https://www.youtube.com/watch?v=zox8ZTFb6VI",
+    tags: ["2020", "Convocation", "SRF Lessons", "English", "Talk", "SRF"]
+  },
+  {
+    id: "b-LzFHT3Y2M",
+    title: "“Deepening Our Practice of Meditation” | A Talk by YSS Sannyasi Swami Smaranananda Giri in English",
+    url: "https://www.youtube.com/watch?v=b-LzFHT3Y2M",
+    tags: ["Swami Smaranananda Giri", "Deepening Meditation", "English"]
+  },
+  {
+    id: "FkWBsufZvz8",
+    title: "“Dynamic Will Can Change Your Life” by Brother Anandamoy",
+    url: "https://www.youtube.com/watch?v=FkWBsufZvz8",
+    tags: ["Brother Anandamoy", "Will Power", "SRF", "English"]
+  },
+  {
+    id: "vS6_aFvX6RE",
+    title: "“Kriya Yoga: The Direct Path to God-Realization”",
+    url: "https://www.youtube.com/watch?v=vS6_aFvX6RE",
+    tags: ["Kriya Yoga", "SRF", "English", "Talk"]
+  },
+  {
+    id: "gN-zC07Y480",
+    title: "“Expanding Your Awareness of God’s Presence”",
+    url: "https://www.youtube.com/watch?v=gN-zC07Y480",
+    tags: ["Presence of God", "YSS", "English", "Meditation"]
+  }
+];
